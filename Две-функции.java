09694b58a992fed63d81405c617d7aedfced504a
shaiurbekuO <shaiurbekuuluomurbek@gmail.com>
2024-01-27 21:52:28 +0600

    Две функции

---
+++ Две-функции.java
@@ -0,0 +1,17 @@
+class Example{
+    public static void main(String[] args){
+        my_first_function();
+        my_first_function();
+        my_first_function();
+        my_second_function();
+        my_second_function();
+    }
+   
+    public static void my_first_function(){
+        System.out.println("Hello world!"); 
+    }
+    public static void my_second_function(){
+        System.out.println("Java the best!"); }
+
+    // пропишите здесь вторую функцию
+}
