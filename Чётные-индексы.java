78a2bd5756e8419e2c6e29db4f0b1b17db3e9680
shaiurbekuO <shaiurbekuuluomurbek@gmail.com>
2024-05-14 12:19:46 +0600

    Чётные индексы

---
+++ Чётные-индексы.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class MyProgram{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        for(int i=0; i<n; i++){
+            array[i] = sc.nextInt();
+        }
+        for(int i=0; i<n; i++){
+            if(i%2==0){
+                System.out.print(array[i]+" ");
+            }
+        }
+    }
+}
